5bb2b98f9730778ab20c969576d47f61797ba8aa
Caleb-Oneyemi <caleb.oneyemi@decagon.dev>
2021-03-18 22:44:32 +0100

    .env changes

--- server/db/connection.js
+++ server/db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const con = await mongoose.connect('mongodb+srv://caleb:caleb@cluster1.rx7lq.mongodb.net/storybooks?retryWrites=true&w=majority', {
+        const con = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
