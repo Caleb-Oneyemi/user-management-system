39027402e66e3f53ec5a8953ba91f709631097a9
Caleb-Oneyemi <caleb.oneyemi@decagon.dev>
2021-03-18 22:23:35 +0100

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const cors = require('cors');
 const indexRouter = require('./server/routes/router');
 const connectDB = require('./server/db/connection');
 
@@ -17,6 +18,7 @@ app.set('view engine', 'ejs');
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.static(path.join(__dirname, './public')));
 
 app.use('/', indexRouter);
